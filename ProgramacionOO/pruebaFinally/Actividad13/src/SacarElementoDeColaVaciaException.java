afe878f6d78947cd7e922420af8f623d8c9288af
jdcar-b <jdom0431@gmail.com>
2019-11-05 12:18:13 +0100

    pruebaFinally -D 13

--- ProgramacionOO/pruebaFinally/Actividad13/src/SacarElementoDeColaVaciaException.java
+++
@@ -1,7 +0,0 @@
-public class SacarElementoDeColaVaciaException extends ArrayIndexOutOfBoundsException {
-
-    public SacarElementoDeColaVaciaException(String message){
-        super(message);
-        System.out.println("La cola está vacia, no se puede sacar ningun elemento.");
-    }
-}
